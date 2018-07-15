19e3f2cbce4ff33f708e7a9b698226081040372b
Andrew Mitchell <andrewpmitchell7@gmail.com>
2018-07-15 11:16:01 -0400

    implemented sql schema

--- server/moviesSchema.sql
+++ server/moviesSchema.sql
@@ -1,1 +1,19 @@
--- SET UP SCHEMA HERE
+CREATE DATABASE badmovies;
+
+USE badmovies;
+
+CREATE TABLE movies (
+  id INTEGER PRIMARY KEY AUTO_INCREMENT,
+  title TEXT,
+  genre TEXT,
+  rating INTEGER
+);
+
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY AUTO_INCREMENT,
+  movieid INTEGER,
+  FOREIGN KEY (movieid)
+    REFERENCES movies(id)
+    ON DELETE CASCADE
+);
+
